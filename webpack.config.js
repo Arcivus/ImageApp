c9f0577eb181f82ebf2f898ba64694aadf6006c4
Sergei <arcivus@gmail.com>
2016-07-07 11:27:47 +0300

    second try

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,13 @@
-const path = require('path')
-const webpack = require('webpack')
-
 
 module.exports = {
-  devtool: 'source-map',
-  
   entry: [
     './src/index.js'
   ],
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/public/'
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
   },
-
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
-  ],
-
   module: {
     loaders: [{
       exclude: /node_modules/,
