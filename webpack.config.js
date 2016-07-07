2bbadc4f61f18064e0b2d5c85179aeaf95caab7b
Sergei <arcivus@gmail.com>
2016-07-07 10:37:32 +0300

    build app

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,32 @@
+const path = require('path')
+const webpack = require('webpack')
+
 
 module.exports = {
   entry: [
     './src/index.js'
   ],
   output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
   },
+
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+
   module: {
     loaders: [{
       exclude: /node_modules/,
